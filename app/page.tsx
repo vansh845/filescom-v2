d847425c1ae95ac6ac0a57221906b4479c585fac
vansh845 <103567533+vansh845@users.noreply.github.com>
2024-02-21 09:38:22 +0530

    home route made client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,35 @@
+'use client'
 import { Deletefile } from "@/components/deletebutton";
-import { Card } from "@/components/ui/card";
 import { filesArray, folderArray } from "@/types";
 import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 
 
-export default async function Home() {
+export default function Home() {
 
-  const res = await axios.get('http://localhost:3001/getFiles?userid=1&prefix=/');
-  const resFolder = await axios.get('http://localhost:3001/listFolders?userid=1&prefix=/');
-  const files = res.data as filesArray[]
-  const folders = resFolder.data as folderArray[]
+  const [files, setFiles] = useState<filesArray[]>();
+  const [folders, setFolders] = useState<folderArray[]>();
+
+  useEffect(() => {
+    async function fetchData() {
+
+      const res = await axios.get('http://localhost:3001/getFiles?userid=1&prefix=/');
+      const resFolder = await axios.get('http://localhost:3001/listFolders?userid=1&prefix=/');
+      setFiles(res.data);
+      setFolders(resFolder.data)
+    }
+    fetchData();
+
+  }, [])
+
   return (
     <main className="flex flex-col">
 
-
-      {folders.map((folder, index) => <Link className="border-b my-2 py-2 ml-2" href={`/folder/${folder.foldername}`} key={index}>{folder.foldername}</Link>)}
-      {files.map((file, index) => <div key={index} className="border-b my-2 py-2 ml-2 flex"><div className="flex justify-between w-full"><a href={file.fileurl}>{file.filename}</a> <Deletefile filename={file.filename} /></div></div>)}
+      {folders?.map((folder, index) => <Link className="border-b my-2 py-2 ml-2" href={`/folder/${folder.foldername}`} key={index}>{folder.foldername}</Link>)}
+      {files?.map((file, index) => <div key={index} className="border-b my-2 py-2 ml-2 flex"><div className="flex justify-between w-full"><a href={file.fileurl}>{file.filename}</a> <Deletefile filename={file.filename} /></div></div>)}
 
     </main>
   )
